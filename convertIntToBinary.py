8e0e82131079cfdff64c6a1e8b98d8991d27768b
Akshay Kumar <akkiakshay440@gmail.com>
2022-12-02 22:00:35 +0530

    binary conversion

---
+++ convertIntToBinary.py
@@ -0,0 +1,14 @@
+for i in range(int(input())):
+    n= int(input())
+    s = ""
+    if 1<=n<=100:
+        b = bin(n)
+        for i in str(b)[2:]:
+            if i == "0":
+                s += "1"
+            else:
+                s += "0"
+        s = int(s,2)
+    else:
+        s = "wrong input"
+    print(s)
